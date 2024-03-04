8fe98d32c0ecb9c426048975119850e41b7104c5
oussama.hassani <oussama.hassani@chifco.com>
2024-03-04 10:11:15 +0100

    add ai cv parser

--- bakend/middlewares/upload.js
+++ bakend/middlewares/upload.js
@@ -4,10 +4,17 @@ const storage = multer.diskStorage({
         cb(null, './storages');
     },
     filename: function (req, file, cb) {
-     //   console.log('file', file)
+        //   console.log('file', file)
         cb(null, new Date().toISOString().replace(/:/g, '-') + (file.originalname.replace(/ /g, '')))
     }
 });
-module.exports = multer({
-    storage: storage,
-})
+
+const upload = multer({ storage: storage });
+
+// Middleware for handling file uploads
+const handleFileUpload = upload.single('file'); // Assuming 'file' is the field name
+
+module.exports = {
+    upload: upload,
+    handleFileUpload: handleFileUpload
+};
---
+++ bakend/server.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { Server } = require("socket.io");
+const http = require("http");
+const path = require('path');
+const fileUploadMiddleware = require('./middlewares/auth');
+
+app.use(cors());
+app.use(express.json());
+require('dotenv').config()
+app.use(express.urlencoded({ extended: true }));
+const db = require("./config/database");
+
+const userrouter = require('./routes/users.routes');
+
+
+
+
+
+
+app.use('/users', userrouter)
+
+
+
+app.get("/user/:user", (req, res) => {
+  let user = req.params.user
+  res.json({ message: "Hii " + user });
+});
+// Define the upload route
+app.post('/api/affinda/resume/upload', fileUploadMiddleware.handleFileUpload, (req, res) => {
+  // Read the file from the uploads folder
+  const filePath = req.file.path;
+
+  // Set up the options for the Affinda API request
+  const options = {
+    method: 'POST',
+    url: 'https://api.affinda.com/v3/documents',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'multipart/form-data; boundary=---011000010111000001101001',
+      authorization: `Bearer ${process.env.KEY}`
+    },
+    formData: {
+      wait: 'true',
+      file: {
+        value: fs.createReadStream(filePath),
+        options: { filename: req.file.originalname, contentType: req.file.mimetype }
+      },
+      collection: 'vocqDuAi',
+      workspace: 'yqeFohQi'
+    }
+  };
+
+  // Make the request to the Affinda API
+  request(options, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send({ error: 'An error occurred while processing the file.' });
+    } else {
+      // Parse the response body as JSON
+      const data = JSON.parse(body);
+
+      // Send the parsed data back to the client
+      res.send(data);
+    }
+  });
+});
+app.use('/file', express.static(path.join(__dirname, 'public')));
+
+
+
+
+const PORT = process.env.PORT || 3333;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}. `);
+});
+
+//Socket IO logic
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("join_room", (room_id) => {
+    socket.join(room_id);
+  });
+
+  socket.on("send_message", async (data) => {
+    //const postRes = await postMessage(data);
+
+    socket.to(data.chatId).emit("receive_message", "hello");
+  });
+});
